967f39eb4c9061cd7ad6a3e5be24a6ddb720f008
dev-juri <ojurifemi132@gmail.com>
2022-05-25 22:04:31 +0100

    chore: project details screen implemented.

---
+++ app/src/main/java/com/earthmovers/www/ui/home/ProjectDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.earthmovers.www.ui.home
+
+import com.earthmovers.www.R
+import com.earthmovers.www.utils.BaseFragment
+
+class ProjectDetailsFragment : BaseFragment(R.layout.fragment_project_details) {
+}
