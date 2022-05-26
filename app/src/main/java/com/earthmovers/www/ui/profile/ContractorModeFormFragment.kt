315dc55c33e5ff789a0ec8a00e71b1192cd7114e
dev-juri <ojurifemi132@gmail.com>
2022-05-26 17:19:08 +0100

    chore: implement contractor mode form screen

---
+++ app/src/main/java/com/earthmovers/www/ui/profile/ContractorModeFormFragment.kt
@@ -0,0 +1,7 @@
+package com.earthmovers.www.ui.profile
+
+import com.earthmovers.www.R
+import com.earthmovers.www.utils.BaseFragment
+
+class ContractorModeFormFragment: BaseFragment(R.layout.fragment_contractor_mode_form) {
+}
--- app/src/main/java/com/earthmovers/www/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/earthmovers/www/ui/profile/ProfileFragment.kt
@@ -1,7 +1,23 @@
 package com.earthmovers.www.ui.profile
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.earthmovers.www.R
 import com.earthmovers.www.utils.BottomNavTopLevelFragment
+import com.earthmovers.www.utils.viewBinding
+import com.earthmovers.www.databinding.FragmentProfileBinding
 
 class ProfileFragment: BottomNavTopLevelFragment(R.layout.fragment_profile) {
+
+    private val binding by viewBinding(FragmentProfileBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.switchMode.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_contractorModeFormFragment)
+        }
+
+    }
 }
