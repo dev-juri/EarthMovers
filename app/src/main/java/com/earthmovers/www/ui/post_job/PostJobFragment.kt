d7e814b0231ed1ba43006cc4f93cba993db1e10a
dev-juri <ojurifemi132@gmail.com>
2022-05-29 00:50:37 +0100

    chore: upload image design

--- app/src/main/java/com/earthmovers/www/ui/post_job/PostJobFragment.kt
+++ app/src/main/java/com/earthmovers/www/ui/post_job/PostJobFragment.kt
@@ -1,7 +1,38 @@
 package com.earthmovers.www.ui.post_job
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import com.earthmovers.www.R
+import com.earthmovers.www.databinding.FragmentPostJobBinding
 import com.earthmovers.www.utils.BottomNavTopLevelFragment
+import com.earthmovers.www.utils.viewBinding
+import java.io.File
 
 class PostJobFragment: BottomNavTopLevelFragment(R.layout.fragment_post_job) {
+
+    private val binding by viewBinding(FragmentPostJobBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data?.data
+                binding.selectedImagePreview.setImageURI(data)
+            }
+        }
+
+        binding.uploadImage.setOnClickListener {
+            val intent = Intent()
+            intent.apply {
+                type = "image/*"
+                action = Intent.ACTION_GET_CONTENT
+            }
+            imageResultLauncher.launch(intent)
+
+        }
+    }
 }
--- app/src/main/java/com/earthmovers/www/ui/profile/ContractorModeFormFragment.kt
+++ app/src/main/java/com/earthmovers/www/ui/profile/ContractorModeFormFragment.kt
@@ -1,7 +1,17 @@
 package com.earthmovers.www.ui.profile
 
+import android.os.Bundle
+import android.view.View
 import com.earthmovers.www.R
+import com.earthmovers.www.databinding.FragmentContractorModeFormBinding
 import com.earthmovers.www.utils.BaseFragment
+import com.earthmovers.www.utils.viewBinding
 
 class ContractorModeFormFragment: BaseFragment(R.layout.fragment_contractor_mode_form) {
+
+    private val binding by viewBinding(FragmentContractorModeFormBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
