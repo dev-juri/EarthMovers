ff99d4e1a7106e76f00541305d9970e7b9d1afc2
dev-juri <ojurifemi132@gmail.com>
2022-06-11 10:36:51 +0100

    Project location on map done

--- app/src/main/java/com/earthmovers/www/ui/home/MapsFragment.kt
+++ app/src/main/java/com/earthmovers/www/ui/home/MapsFragment.kt
@@ -1,5 +1,7 @@
 package com.earthmovers.www.ui.home
 
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -9,7 +11,6 @@ import com.earthmovers.www.databinding.FragmentMapsBinding
 import com.earthmovers.www.utils.BaseFragment
 import com.earthmovers.www.utils.viewBinding
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -22,29 +23,33 @@ class MapsFragment : BaseFragment(R.layout.fragment_maps) {
     private val binding by viewBinding(FragmentMapsBinding::bind)
     private val viewModel by activityViewModels<HomeViewModel>()
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val coder = Geocoder(requireContext())
+        var address: List<Address>
+
         viewModel.selectedPost.observe(viewLifecycleOwner) {
-            Toast.makeText(requireContext(), it.location.toString(), Toast.LENGTH_SHORT).show()
+            try {
+                address = coder.getFromLocationName(it.location, 5)
+                val location = address[0]
+                val lat = location.latitude
+                val long = location.longitude
+                val locationOfProject = LatLng(lat, long)
+
+                val callback = OnMapReadyCallback { googleMap ->
+
+                    googleMap.addMarker(
+                        MarkerOptions().position(locationOfProject).title("Project Location")
+                    )
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(locationOfProject))
+                }
+                val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+                mapFragment?.getMapAsync(callback)
+            } catch (e: Exception) {
+                Toast.makeText(requireContext(), "Couldn't find address", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
     }
 }
