5c5b67dce31d883eb6ab38a7c80e7bfa39fdbb70
dev-juri <ojurifemi132@gmail.com>
2022-06-12 22:52:47 +0100

    Last location done

--- app/src/main/java/com/earthmovers/www/ui/home/MapsFragment.kt
+++ app/src/main/java/com/earthmovers/www/ui/home/MapsFragment.kt
@@ -1,16 +1,22 @@
 package com.earthmovers.www.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.activityViewModels
 import com.earthmovers.www.R
-import com.earthmovers.www.databinding.FragmentMapsBinding
 import com.earthmovers.www.utils.BaseFragment
-import com.earthmovers.www.utils.viewBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -20,12 +26,17 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MapsFragment : BaseFragment(R.layout.fragment_maps) {
 
-    private val binding by viewBinding(FragmentMapsBinding::bind)
     private val viewModel by activityViewModels<HomeViewModel>()
+    private lateinit var googleMap: GoogleMap
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        fusedLocationClient =
+            LocationServices.getFusedLocationProviderClient(context!!)
+        
         val coder = Geocoder(requireContext())
         var address: List<Address>
 
@@ -37,19 +48,63 @@ class MapsFragment : BaseFragment(R.layout.fragment_maps) {
                 val long = location.longitude
                 val locationOfProject = LatLng(lat, long)
 
-                val callback = OnMapReadyCallback { googleMap ->
+                val callback = OnMapReadyCallback { p0 ->
+
+                    googleMap = p0
+                    googleMap.uiSettings.isZoomControlsEnabled = true
+
+                    setUpMap()
 
                     googleMap.addMarker(
                         MarkerOptions().position(locationOfProject).title("Project Location")
                     )
-                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(locationOfProject))
+                    //googleMap.moveCamera(CameraUpdateFactory.newLatLng(locationOfProject))
                 }
-                val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+                val mapFragment =
+                    childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
                 mapFragment?.getMapAsync(callback)
             } catch (e: Exception) {
                 Toast.makeText(requireContext(), "Couldn't find address", Toast.LENGTH_SHORT).show()
             }
         }
 
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                (activity as AppCompatActivity),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE
+            )
+            return
+        }
+
+        googleMap.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = location
+                val currentLatLong = LatLng(location.latitude, location.longitude)
+
+                googleMap.addMarker(
+                    MarkerOptions().position(currentLatLong).title("Current Location")
+                )
+                Toast.makeText(requireContext(), location.toString(), Toast.LENGTH_SHORT).show()
+
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
+            } else {
+                Toast.makeText(requireContext(), "Location is null", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 1
     }
 }
